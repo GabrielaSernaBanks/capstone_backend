69dd2ec6b9fb0a8603bcdb140f5d514f1dcd8f7c
GabrielaSernaBanks <gserna88@gmail.com>
2023-05-16 15:57:50 -0400

    Editing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 const therapistsRoute = require('./routes/therapists');
 const casesRoute = require('./routes/cases');
