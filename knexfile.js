08a6ba38ee0d61e7ec1376b3f0e29bb44cb07c93
GabrielaSernaBanks <gserna88@gmail.com>
2023-05-17 11:26:47 -0400

    Added .env file

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,30 @@
-// Update with your config settings.
+// // Update with your config settings.
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
+// /**
+//  * @type { Object.<string, import("knex").Knex.Config> }
+//  */
+// module.exports = {
+
+//     client: 'mysql',
+//     connection: {
+//       host: '127.0.0.1',
+//       user: 'root',
+//       password: "Srotag11!!",
+//       database: 'therapists',
+//       charset: "utf8"
+//   },
+// };
+
+
+require('dotenv').config();
+
 module.exports = {
-
-    client: 'mysql',
-    connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: "Srotag11!!",
-      database: 'therapists',
+  client: "mysql",
+  connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       charset: "utf8"
-  },
-
-  
+  }
 };
